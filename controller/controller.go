10093e2f1c6a5361642ffe1ca528146bf7a7161f
Bart Spaans <bart.spaans@gmail.com>
2019-03-20 17:46:57 +0000

    Document controller

--- controller/controller.go
+++ controller/controller.go
@@ -7,6 +7,8 @@ import (
 	"github.com/bspaans/bs8bs/synth"
 )
 
+// The Controller is a high level object that can be used to setup
+// and control the Synthesizer, Sequencer and the instrument banks.
 type Controller struct {
 	Config             *audio.AudioConfig
 	Synth              *synth.Synth
@@ -22,19 +24,24 @@ func NewController(cfg *audio.AudioConfig) *Controller {
 	}
 }
 
-func (c *Controller) EnableWavSink(file string) error {
-	return c.Synth.EnableWavSink(file)
-}
-
+// Enables "real-time" audio output using the PortAudio library.
 func (c *Controller) EnablePortAudioSink() error {
 	return c.Synth.EnablePortAudioSink()
 }
 
+// Record all Synthesizer output in to a .wav file. The file gets written when
+// the sink closes (usually when the program ends).
+func (c *Controller) EnableWavSink(file string) error {
+	return c.Synth.EnableWavSink(file)
+}
+
+// Load an instrument bank definition from a file.
 func (c *Controller) LoadInstrumentBank(file string) error {
 	c.InstrumentBankFile = file
 	return c.ReloadInstrumentBank()
 }
 
+// Reload the instrument bank by reloading the file.
 func (c *Controller) ReloadInstrumentBank() error {
 	if c.InstrumentBankFile != "" {
 		return c.Synth.LoadInstrumentBank(c.InstrumentBankFile)
@@ -42,11 +49,13 @@ func (c *Controller) ReloadInstrumentBank() error {
 	return nil
 }
 
+// Load an percussion instrument bank definition from a file.
 func (c *Controller) LoadPercussionBank(file string) error {
 	c.PercussionBankFile = file
 	return c.ReloadPercussionBank()
 }
 
+// Reload the instrument percussion bank by reloading the file.
 func (c *Controller) ReloadPercussionBank() error {
 	if c.PercussionBankFile != "" {
 		return c.Synth.LoadPercussionBank(c.PercussionBankFile)
@@ -54,6 +63,8 @@ func (c *Controller) ReloadPercussionBank() error {
 	return nil
 }
 
+// Load a Sequencer definition from a file and start the sequencer in a
+// go-routine.
 func (c *Controller) LoadSequencerFromFile(file string) error {
 	seq, err := sequencer.NewSequencerFromFile(file)
 	if err != nil {
@@ -64,24 +75,31 @@ func (c *Controller) LoadSequencerFromFile(file string) error {
 	return nil
 }
 
-func (c *Controller) StartSynth() {
-	c.Synth.Start()
-}
-
-func (c *Controller) StartSequencer() {
-	c.Sequencer.Start(c.Synth.Inputs)
-}
-
-func (c *Controller) StartVirtualMIDIDevice() {
-	midi.StartVirtualMIDIDevice(c.Synth.Inputs)
-}
-
+// Reload the sequencer by reloading the file.
 func (c *Controller) ReloadSequencer() {
 	if c.Sequencer != nil {
 		c.Sequencer.Reload()
 	}
 }
 
+// Start Synthesizer. Note that this synthesizer isn't run in a go-routine by
+// default.
+func (c *Controller) StartSynth() {
+	c.Synth.Start()
+}
+
+// Start Sequencer. The sequencer is started in its own go-routine.
+func (c *Controller) StartSequencer() {
+	c.Sequencer.Start(c.Synth.Inputs)
+}
+
+// Register this program as a Virtual MIDI device. Programs that support it
+// can then output MIDI to the device.
+func (c *Controller) StartVirtualMIDIDevice() {
+	midi.StartVirtualMIDIDevice(c.Synth.Inputs)
+}
+
+// Close the Synthesizer and its sinks.
 func (c *Controller) Quit() {
 	c.Synth.Close()
 }
