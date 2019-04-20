a81a12cce87b2368c3b6eb1a17099415dae14ba2
Bart Spaans <bart.spaans@gmail.com>
2019-04-20 18:16:34 +0100

    Derive (faulty) frequency and (correct) amplitude envelopes from peak track (wip)

--- spectral/peak_track.go
+++ spectral/peak_track.go
@@ -1,6 +1,10 @@
 package spectral
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+	"math/cmplx"
+)
 
 // Tracks a SpectralPeak over time. For example: this could track the
 // frequency bin around 440.0Hz over time. It is usually constructed
@@ -35,13 +39,35 @@ func (s *SpectralPeakTrack) Add(peak *SpectralPeak) {
 }
 
 func (s *SpectralPeakTrack) String() string {
+	fmt.Println(s.FrequencyEnvelope())
 	return fmt.Sprintf("Spectral Peak Track starting at frame %d %.2fHz [%d] - %d frames", s.FrameIndex, s.Frequency, s.Index, len(s.Track))
 }
 
 func (s *SpectralPeakTrack) FrequencyEnvelope() []float64 {
-	return []float64{}
+	result := make([]float64, len(s.Track))
+	div := math.Pi * 2 * (1024.0 - 256.0)
+	for i, peak := range s.Track {
+		// skip the first value
+		if i == 0 {
+			continue
+		}
+		prevRadian := cmplx.Phase(complex128(s.Track[i-1].Value))
+		radian := cmplx.Phase(complex128(peak.Value))
+		// TODO: this doesn't work because we're dealing with wrapped phase (-pi <-> pi)
+		// see: https://en.wikipedia.org/wiki/Instantaneous_phase
+		diff := math.Abs(radian - prevRadian)
+		velocty := diff / (1024.0 - 256.0)
+		freq := velocty * 44100 / (math.Pi * 2)
+		fmt.Printf("%.3f radian/sample %.2fHz %.2fHz\n", velocty, freq, freq+s.Frequency)
+		result[i] = (radian - prevRadian) / div
+	}
+	return result
 }
 
 func (s *SpectralPeakTrack) AmplitudeEnvelope() []float64 {
-	return []float64{}
+	result := make([]float64, len(s.Track))
+	for i, peak := range s.Track {
+		result[i] = cmplx.Abs(complex128(peak.Value))
+	}
+	return result
 }
