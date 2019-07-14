602dc8bc21cda30fcfb189d6910bb1811c7061fa
Bart Spaans <bart.spaans@gmail.com>
2019-07-14 18:46:13 +0100

    Add dummy device for darwin

---
+++ midi/device_darwin.go
@@ -0,0 +1,11 @@
+package midi
+
+import (
+	"fmt"
+
+	"github.com/bspaans/bleep/synth"
+)
+
+func StartVirtualMIDIDevice(s chan *synth.Event) {
+	fmt.Println("Virtual MIDI device not supported in CGO builds on MacOS.")
+}
